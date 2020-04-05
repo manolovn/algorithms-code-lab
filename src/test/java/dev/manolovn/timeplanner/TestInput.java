6b35b9d4ef6a171b0250fc9fc4bcd13208c3d52e
mvera <mvera@tuenti.com>
2020-04-05 18:56:54 +0200

    added test cases to time planner

--- src/test/java/dev/manolovn/timeplanner/TestInput.java
+++ src/test/java/dev/manolovn/timeplanner/TestInput.java
@@ -8,6 +8,60 @@ class TestInput {
         int duration = 8;
         int[] expected = new int[]{60, 68};
 
-        return new Object[]{ slotsA, slotsB, duration, expected};
+        return new Object[]{slotsA, slotsB, duration, expected};
+    }
+
+    static Object[] input1() {
+        int[][] slotsA = new int[][]{new int[]{7, 12}};
+        int[][] slotsB = new int[][]{new int[]{2, 11}};
+        int duration = 5;
+        int[] expected = new int[]{};
+
+        return new Object[]{slotsA, slotsB, duration, expected};
+    }
+
+    static Object[] input2() {
+        int[][] slotsA = new int[][]{new int[]{6, 12}};
+        int[][] slotsB = new int[][]{new int[]{2, 11}};
+        int duration = 5;
+        int[] expected = new int[]{6, 11};
+
+        return new Object[]{slotsA, slotsB, duration, expected};
+    }
+
+    static Object[] input3() {
+        int[][] slotsA = new int[][]{new int[]{1, 10}};
+        int[][] slotsB = new int[][]{new int[]{2, 3}, new int[]{5, 7}};
+        int duration = 2;
+        int[] expected = new int[]{5, 7};
+
+        return new Object[]{slotsA, slotsB, duration, expected};
+    }
+
+    static Object[] input4() {
+        int[][] slotsA = new int[][]{new int[]{0, 5}, new int[]{50, 70}, new int[]{120, 125}};
+        int[][] slotsB = new int[][]{new int[]{0, 50}};
+        int duration = 8;
+        int[] expected = new int[]{};
+
+        return new Object[]{slotsA, slotsB, duration, expected};
+    }
+
+    static Object[] input5() {
+        int[][] slotsA = new int[][]{new int[]{10, 50}, new int[]{60, 120}, new int[]{140, 210}};
+        int[][] slotsB = new int[][]{new int[]{0, 15}, new int[]{60, 70}};
+        int duration = 8;
+        int[] expected = new int[]{60, 68};
+
+        return new Object[]{slotsA, slotsB, duration, expected};
+    }
+
+    static Object[] input6() {
+        int[][] slotsA = new int[][]{new int[]{10, 50}, new int[]{60, 120}, new int[]{140, 210}};
+        int[][] slotsB = new int[][]{new int[]{0, 15}, new int[]{60, 72}};
+        int duration = 12;
+        int[] expected = new int[]{60, 72};
+
+        return new Object[]{slotsA, slotsB, duration, expected};
     }
 }
--- src/test/java/dev/manolovn/timeplanner/TimePlannerTest.java
+++ src/test/java/dev/manolovn/timeplanner/TimePlannerTest.java
@@ -15,9 +15,15 @@ public class TimePlannerTest {
 
     @Parameters(name = "{index}: meetingPlanner({0},{1},{2})={3}")
     public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
+        return Arrays.asList(
                 TestInput.input0(),
-        });
+                TestInput.input1(),
+                TestInput.input2(),
+                TestInput.input3(),
+                TestInput.input4(),
+                TestInput.input5(),
+                TestInput.input6()
+        );
     }
 
     private int[][] slotsA;
