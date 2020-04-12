f7b6adf71c0fd8231cdd961ffc28366b9631a553
mvera <mvera@tuenti.com>
2020-04-13 00:55:32 +0200

    problem added: award budget cuts

---
+++ src/main/java/dev/manolovn/awardbudgetcuts/AwardBudgetCuts.java
@@ -0,0 +1,32 @@
+package dev.manolovn.awardbudgetcuts;
+
+import java.util.Arrays;
+
+public class AwardBudgetCuts {
+
+    /*
+      [2, 100, 50, 120, 1000], newBudget = 190
+
+      [2, 50, 100, 120, 1000]
+
+      1. sort reversed
+      2. find the grantsArray <= newBudget --> sum() = sumGA; extract index
+      3. divide newBudget (sumGA) / grantsArray.length - index
+    */
+    static double findGrantsCap(double[] grantsArray, double newBudget) {
+        Arrays.sort(grantsArray);
+        double sum = 0, cup = 0;
+        for (int i = 0; i < grantsArray.length; i++) {
+            sum += grantsArray[i];
+            if ((newBudget - sum) / grantsArray[i] >= (grantsArray.length - i - 1)) {
+                cup = (newBudget - sum) / (grantsArray.length - i - 1);
+            } else {
+                break;
+            }
+        }
+        if (cup == 0.0) {
+            cup = newBudget / grantsArray.length;
+        }
+        return cup;
+    }
+}
---
+++ src/test/java/dev/manolovn/awardbudgetcuts/AwardBudgetCutsTest.java
@@ -0,0 +1,43 @@
+package dev.manolovn.awardbudgetcuts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class AwardBudgetCutsTest {
+
+    @Parameters(name = "{index}: findGrantsCap({0})={1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new double[]{2, 4}, 3, 1.5},
+                {new double[]{2, 4, 6}, 3, 1},
+                {new double[]{2, 100, 50, 120, 167}, 400, 128},
+                {new double[]{2, 100, 50, 120, 1000}, 190, 47},
+                {new double[]{21, 100, 50, 120, 130, 110}, 140, 23.8},
+                {new double[]{210, 200, 150, 193, 130, 110, 209, 342, 117}, 1530, 211},
+        });
+    }
+
+    private double[] input;
+    private double newBudget;
+    private double expected;
+
+    public AwardBudgetCutsTest(double[] input, double newBudget, double expected) {
+        this.input = input;
+        this.newBudget = newBudget;
+        this.expected = expected;
+    }
+
+    @Test
+    public void test() {
+        double output = AwardBudgetCuts.findGrantsCap(input, newBudget);
+        assertEquals(expected, output, 0.0);
+    }
+}
