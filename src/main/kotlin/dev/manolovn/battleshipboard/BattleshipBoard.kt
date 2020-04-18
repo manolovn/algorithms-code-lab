af23318ce0ce9e82c7cfb84780a89534ea50a2e9
mvera <mvera@tuenti.com>
2020-04-18 20:53:33 +0200

    fix typos in battleship board comment

--- src/main/kotlin/dev/manolovn/battleshipboard/BattleshipBoard.kt
+++ src/main/kotlin/dev/manolovn/battleshipboard/BattleshipBoard.kt
@@ -6,10 +6,10 @@ object BattleshipBoard {
         Seems to be classic problem oriented to bfs but taking into account that we should not
         modify the input; a classic bfs implementation that disable the visited nodes is not enough.
         - we could use a list of visited nodes by our bfs as a blacklist, with this approach we accomplish
-        the not modifying input requirement but we also need to think that space complexity must to be O(1)
+        the not modifying input requirement but we also need to think that space complexity must be O(1)
         - the statement says that we should do only one pass and reviewing the conditions of how the ships
-        are drawn we could use a "greedy" approach where a ship is defined by a cell with X and neighbours
-        distinct of X. That's approach is valid because the ships only could be in horizontal or vertical
+        are drawn we could use a "greedy" approach where a ship is defined by a cell with "X" and neighbours
+        distinct of "X". That's approach is valid because the ships only could be in horizontal or vertical
         orientation
 
          Time : O(N*M) ; where N and M are the dimensions of the matrix
