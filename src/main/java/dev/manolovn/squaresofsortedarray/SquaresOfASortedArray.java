64af50ec4f62d8d5b4f65ffb6e47031c9839cfab
manolovn <manolo.vn@gmail.com>
2020-08-11 22:54:50 +0200

    problem added: squares of a sorted array - alternative solution

--- src/main/java/dev/manolovn/squaresofsortedarray/SquaresOfASortedArray.java
+++ src/main/java/dev/manolovn/squaresofsortedarray/SquaresOfASortedArray.java
@@ -1,5 +1,7 @@
 package dev.manolovn.squaresofsortedarray;
 
+import java.util.Arrays;
+
 /**
  * Given an array of integers A sorted in non-decreasing order,
  * return an array of the squares of each number, also in sorted non-decreasing order.
@@ -37,6 +39,25 @@ public class SquaresOfASortedArray {
                 tail--;
             }
         }
+        return solution;
+    }
+
+    /**
+     * Time:  O(nlogn)
+     * Space: O(n)
+     */
+    public int[] sortedSquaresLessEfficient(int[] A) {
+        if (A == null) {
+            return null;
+        }
+
+        int[] solution = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            solution[i] = A[i] * A[i];
+        }
+
+        Arrays.sort(solution);
+
         return solution;
     }
 }
