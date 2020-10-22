f8e04059bb824d1bee4ef7faeab831a8dfb37d5b
manolovn <manolo.vn@gmail.com>
2020-10-22 18:03:17 +0200

    problem added: find the duplicated number

---
+++ src/main/java/dev/manolovn/findtheduplicates/FindTheDuplicateNumber.java
@@ -0,0 +1,35 @@
+package dev.manolovn.findtheduplicates;
+
+/**
+ * We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’.
+ * The array has only one duplicate but it can be repeated multiple times.
+ * Find that duplicate number without using any extra space. You are, however, allowed to modify the input array.
+ * <p>
+ * Example:
+ * Input: [1, 4, 4, 3, 2]
+ * Output: 4
+ */
+public class FindTheDuplicateNumber {
+
+    public static int findNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] != i + 1) {
+                if (nums[i] != nums[nums[i] - 1]) {
+                    swap(nums, nums[i] - 1, i);
+                } else {
+                    return nums[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int t = arr[a];
+        arr[a] = arr[b];
+        arr[b] = t;
+    }
+}
