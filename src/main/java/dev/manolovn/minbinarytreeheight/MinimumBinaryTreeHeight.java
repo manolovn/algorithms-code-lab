d4fc7cfe4c7a8ec4116db79ebc27efc5dedf0fef
manolovn <manolo.vn@gmail.com>
2020-10-24 01:30:07 +0200

    problem added: minimum binary tree depth

---
+++ src/main/java/dev/manolovn/minbinarytreeheight/MinimumBinaryTreeHeight.java
@@ -0,0 +1,51 @@
+package dev.manolovn.minbinarytreeheight;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Find the minimum depth of a binary tree. The minimum depth is the number of nodes
+ * along the shortest path from the root node to the nearest leaf node
+ *
+ * Difficulty: EASY
+ * Topics: BFS
+ */
+class MinimumBinaryTreeHeight {
+
+    /**
+     * Time:  O(n)
+     * Space: O(1)
+     */
+    public static int findDepth(TreeNode root) {
+        int minHeight = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            minHeight++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode c = q.poll();
+                if (c.right == null && c.left == null) {
+                    return minHeight;
+                }
+                if (c.left != null) {
+                    q.offer(c.left);
+                }
+                if (c.right != null) {
+                    q.offer(c.right);
+                }
+            }
+        }
+        return minHeight;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
