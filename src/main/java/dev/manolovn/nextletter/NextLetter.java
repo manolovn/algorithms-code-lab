ee7b400763e8cfbf68b43b1bb8367cac3a33ca31
manolovn <manolo.vn@gmail.com>
2020-10-31 18:19:02 +0100

    problem added: next letter

---
+++ src/main/java/dev/manolovn/nextletter/NextLetter.java
@@ -0,0 +1,40 @@
+package dev.manolovn.nextletter;
+
+/**
+ * Given an array of lowercase letters sorted in ascending order, find the smallest letter in the given array
+ * greater than a given ‘key’.
+ * Assume the given array is a circular list, which means that the last letter is assumed to be connected
+ * with the first letter. This also means that the smallest letter in the given array is greater than the
+ * last letter of the array and is also the first letter of the array.
+ *
+ * Example:
+ * Input: ['a', 'c', 'f', 'h'], key = 'f'
+ * Output: 'h'
+ * Explanation: The smallest letter greater than 'f' is 'h' in the given array
+ *
+ * difficulty: MEDIUM
+ * topics: BINARY SEARCH
+ */
+public class NextLetter {
+
+    /**
+     * Time:  O(logn)
+     * Spcae: O(1)
+     */
+    public static char searchNextLetter(char[] letters, char key) {
+        int n = letters.length;
+
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (key < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return letters[start % n];
+    }
+}
