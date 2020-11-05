1bfb5834ec5abc99581a292d57fdeb133936e84e
manolovn <manolo.vn@gmail.com>
2020-11-05 01:11:39 +0100

    problem added: subset sum - improved memory

--- src/main/java/dev/manolovn/subsetsum/SubsetSum.java
+++ src/main/java/dev/manolovn/subsetsum/SubsetSum.java
@@ -41,4 +41,32 @@ public class SubsetSum {
         }
         return dp[n-1][sum];
     }
+
+    /**
+     * improved solution with O(s) space complexity
+     */
+    public boolean canPartition_improved(int[] num, int sum) {
+        int n = num.length;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true; // base case
+
+        // with only one number, we can have a subset only when the required sum is equal to its value
+        for (int s = 1; s <= sum; s++) {
+            dp[s] = num[0] == s;
+        }
+
+        // process all subsets for all sums
+        for (int i = 1; i < n; i++) {
+            for (int s = sum; s >= 0; s--) {
+                // if dp[s]==true, this means we can get the sum 's' without num[i], hence we can move on to
+                // the next number else we can include num[i] and see if we can find a subset to get the
+                // remaining sum
+                if (!dp[s] && s >= num[i]) {
+                    dp[s] = dp[s - num[i]];
+                }
+            }
+        }
+
+        return dp[sum];
+    }
 }
