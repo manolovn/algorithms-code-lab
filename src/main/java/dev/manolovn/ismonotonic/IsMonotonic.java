614b356375f207449366ac4b871492e2a15a7686
manolovn <manolo.vn@gmail.com>
2020-11-08 21:59:49 +0100

    problem added: is monotonic

---
+++ src/main/java/dev/manolovn/ismonotonic/IsMonotonic.java
@@ -0,0 +1,41 @@
+package dev.manolovn.ismonotonic;
+
+/**
+ * source: https://leetcode.com/problems/monotonic-array/
+ * difficulty: EASY
+ * topics: ARRAYS
+ */
+public class IsMonotonic {
+
+    /*
+    #1: full traverse
+    t: O(n)
+    s: O(1)
+
+    [1,2,3]
+    [ 1 1 1 1 1 1 6 7]
+
+    */
+    public boolean isMonotonic(int[] A) {
+        // infer direction
+        boolean isAsc = true;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > A[i - 1]) {
+                break;
+            } else if (A[i] < A[i - 1]) {
+                isAsc = false;
+                break;
+            }
+        }
+        // check if the direction is correct
+        for (int i = 1; i < A.length; i++) {
+            if (isAsc) {
+                if (A[i] < A[i - 1]) return false;
+            } else {
+                // is desc
+                if (A[i] > A[i - 1]) return false;
+            }
+        }
+        return true;
+    }
+}
