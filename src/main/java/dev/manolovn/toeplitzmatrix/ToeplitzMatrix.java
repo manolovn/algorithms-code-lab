efd2010147b91396a79f196bdd24dfdd6be5be6c
manolovn <manolo.vn@gmail.com>
2020-11-09 19:57:45 +0100

    problema added: toeplitz matrix - alternative solution added

--- src/main/java/dev/manolovn/toeplitzmatrix/ToeplitzMatrix.java
+++ src/main/java/dev/manolovn/toeplitzmatrix/ToeplitzMatrix.java
@@ -59,4 +59,15 @@ public class ToeplitzMatrix {
         }
         return true;
     }
+
+    /**
+     * More compact alternative approach
+     */
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[i].length; j++)
+                if (i > 0 && j > 0 && matrix[i][j] != matrix[i - 1][j - 1])
+                    return false;
+        return true;
+    }
 }
