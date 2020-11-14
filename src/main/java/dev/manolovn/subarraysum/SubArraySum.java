a2ad04cf330258b64d432bf8f57e1c3aba0d329e
manolovn <manolo.vn@gmail.com>
2020-11-14 01:43:48 +0100

    problem added: subarraysum without space

--- src/main/java/dev/manolovn/subarraysum/SubArraySum.java
+++ src/main/java/dev/manolovn/subarraysum/SubArraySum.java
@@ -18,7 +18,7 @@ public class SubArraySum {
         int count = 0;
         int sum = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(0,1);
+        map.put(0, 1);
 
         for (int num : nums) {
             sum += num;
@@ -28,6 +28,24 @@ public class SubArraySum {
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
 
+        return count;
+    }
+
+    /**
+     * Alternative solution without space
+     * Time:  O(n^2)
+     * Space: O(1)
+     */
+    public int subarraySum_alternative(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end < nums.length; end++) {
+                sum += nums[end];
+                if (sum == k)
+                    count++;
+            }
+        }
         return count;
     }
 }
