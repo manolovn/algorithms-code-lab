9ed4833412dd9690dd6aeb726af02e8d8884e40e
manolovn <manolo.vn@gmail.com>
2020-11-17 21:16:18 +0100

    problem added: decode ways

---
+++ src/main/java/dev/manolovn/decodeways/DecodeWays.java
@@ -0,0 +1,28 @@
+package dev.manolovn.decodeways;
+
+/**
+ * source: https://leetcode.com/problems/decode-ways/
+ * difficulty: MEDIUM
+ * topic: DP
+ */
+public class DecodeWays {
+
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+
+        for (int i = 2; i < s.length() + 1; i++) {
+            if (s.charAt(i-1) != '0') {
+                dp[i] += dp[i-1];
+            }
+
+            int num = ((s.charAt(i-2) - '0') * 10) + (s.charAt(i-1) - '0');
+            if (num <= 26 && num >= 10) {
+                dp[i] += dp[i-2];
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
