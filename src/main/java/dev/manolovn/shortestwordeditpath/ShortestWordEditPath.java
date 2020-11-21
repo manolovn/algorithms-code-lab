c7264837bc1e4d4045a44f04dae9c066e34432ba
manolovn <manolo.vn@gmail.com>
2020-11-21 18:47:02 +0100

    problem added: shortest word edit path - added statement

--- src/main/java/dev/manolovn/shortestwordeditpath/ShortestWordEditPath.java
+++ src/main/java/dev/manolovn/shortestwordeditpath/ShortestWordEditPath.java
@@ -3,6 +3,26 @@ package dev.manolovn.shortestwordeditpath;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Given two words source and target, and a list of words words,
+ * find the length of the shortest series of edits that transforms source to target.
+ * Each edit must change exactly one letter at a time, and each intermediate word
+ * (and the final target word) must exist in words.
+ * If the task is impossible, return -1.
+ *
+ * Examples:
+ * source = "bit", target = "dog"
+ * words = ["but", "put", "big", "pot", "pog", "dog", "lot"]
+ * output: 5
+ * explanation: bit -> but -> put -> pot -> pog -> dog has 5 transitions.
+ *
+ * source = "no", target = "go"
+ * words = ["to"]
+ * output: -1
+ *
+ * difficulty: MEDIUM
+ * topic: BFS + GRAPH + STRINGS
+ */
 public class ShortestWordEditPath {
 
     /**
