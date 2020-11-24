207858427d09236f99909ecc320c9ac66be22dd0
manolovn <manolo.vn@gmail.com>
2020-11-24 15:06:42 +0100

    problem added: length of longest substring - added more solutions

--- src/main/java/dev/manolovn/longestsubstring/LenghtOfLongestSubstring.java
+++ src/main/java/dev/manolovn/longestsubstring/LenghtOfLongestSubstring.java
@@ -1,7 +1,9 @@
 package dev.manolovn.longestsubstring;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * source: https://leetcode.com/problems/longest-substring-without-repeating-characters/
@@ -10,6 +12,14 @@ import java.util.Map;
  */
 public class LenghtOfLongestSubstring {
 
+    /**
+     * Time:  O(n)
+     * Space: O(n) ; this could be constant if we know
+     *               the string charset;
+     *                  26 to characters a-zA-Z
+     *                  128 to ASCII
+     *                  256 to ASCII extended
+     */
     public int lengthOfLongestSubstring(String s) {
         if (s.isEmpty()) return 0;
         Map<Character, Integer> map = new HashMap<>();
@@ -20,6 +30,46 @@ public class LenghtOfLongestSubstring {
             }
             max = Math.max(max, j - i + 1);
             map.put(s.charAt(j), j + 1);
+        }
+        return max;
+    }
+
+    /**
+     * Knowing the current charset of s, we could transform the map
+     * to a table with the fixed size of the character, so we reduce
+     * the space complexity to O(1)
+     */
+    public int lengthOfLongestSubstring_withConstantSpace(String s) {
+        if (s.isEmpty()) return 0;
+        int[] map = new int[128];
+        int max = 0;
+        for (int j = 0, i = 0; j < s.length(); j++) {
+            i = Math.max(i, map[s.charAt(j)]);
+            max = Math.max(max, j - i + 1);
+            map[s.charAt(j)] = j + 1;
+        }
+        return max;
+    }
+
+    /**
+     * Time:  O(n)
+     * Space: O(n)
+     */
+    public int lengthOfLongestSubstring_easier(String s) {
+        if (s.isEmpty()) return 0;
+        Set<Character> set = new HashSet<>();
+        int max = 0;
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < s.length()) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j));
+                max = Math.max(max, j - i);
+                j++;
+            } else {
+                set.add(s.charAt(i));
+                i++;
+            }
         }
         return max;
     }
