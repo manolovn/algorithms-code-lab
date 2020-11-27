92a6b817f97a9af03911275d3e6be78b6c3df4f3
manolovn <manolo.vn@gmail.com>
2020-11-27 12:20:00 +0100

    problem added: rotate matrix - reformated

--- src/main/java/dev/manolovn/rotatematrix/RotateMatrix.java
+++ src/main/java/dev/manolovn/rotatematrix/RotateMatrix.java
@@ -14,7 +14,7 @@ public class RotateMatrix {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        for (int i = 0; i < (n + 1) / 2; i ++) {
+        for (int i = 0; i < (n + 1) / 2; i++) {
             for (int j = 0; j < n / 2; j++) {
                 int temp = matrix[n - 1 - j][i];
                 matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
