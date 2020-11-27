769c3779ad74404417f2136a4731928d03a60968
manolovn <manolo.vn@gmail.com>
2020-11-27 12:17:47 +0100

    problem added: rotate matrix

---
+++ src/main/java/dev/manolovn/rotatematrix/RotateMatrix.java
@@ -0,0 +1,27 @@
+package dev.manolovn.rotatematrix;
+
+/**
+ * source: https://leetcode.com/problems/rotate-image/
+ * difficulty: MEDIUM
+ * topic: ARRAYS
+ */
+public class RotateMatrix {
+
+    /**
+     * Time:  O(n^2)
+     * Space: O(1)
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
