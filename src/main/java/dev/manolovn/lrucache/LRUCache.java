3ae2bcff44e82158298dc3406a8fcaa05f9ee727
manolovn <manolo.vn@gmail.com>
2020-11-28 20:40:12 +0100

    problem added: lru cache

---
+++ src/main/java/dev/manolovn/lrucache/LRUCache.java
@@ -0,0 +1,32 @@
+package dev.manolovn.lrucache;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * source: https://leetcode.com/problems/lru-cache/
+ * difficulty: MEDIUM
+ * topic: DESIGN
+ */
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
