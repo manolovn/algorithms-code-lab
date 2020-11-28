b39769c24c9ca8d1f94e9c9e99dee7d0082e56ed
manolovn <manolo.vn@gmail.com>
2020-11-28 01:12:28 +0100

    problem added: search rotated sorted array

---
+++ src/main/java/dev/manolovn/shiftedarraysearch/SearchInRotatedSortedArray.java
@@ -0,0 +1,57 @@
+package dev.manolovn.shiftedarraysearch;
+
+/**
+ * source: https://leetcode.com/problems/search-in-rotated-sorted-array/
+ * difficulty: MEDIUM
+ * topic: ARRAYS + BINARY SEARCH
+ */
+public class SearchInRotatedSortedArray {
+
+    /**
+     * Time:  O(logn)
+     * Space: O(1)
+     */
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        if (nums.length == 1) return nums[0] == target ? 0 : -1;
+
+        int rp = findRotationPoint(nums);
+
+        if (rp == 0 || target < nums[0]) {
+            return search(nums, target, rp, nums.length - 1);
+        }
+        return search(nums, target, 0, rp - 1);
+    }
+
+    private int findRotationPoint(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        if (nums[lo] < nums[hi]) return 0;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                return mid + 1;
+            } else if (nums[mid] < nums[lo]) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return 0;
+    }
+
+    private int search(int[] nums, int target, int lo, int hi) {
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
