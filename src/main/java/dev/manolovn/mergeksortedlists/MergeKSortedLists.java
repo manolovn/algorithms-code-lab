70b50998046ce5be5cbb1e6da6269dd2afe4c170
manolovn <manolo.vn@gmail.com>
2020-11-28 19:40:33 +0100

    updated solution merge k sorted lists

--- src/main/java/dev/manolovn/mergeksortedlists/MergeKSortedLists.java
+++ src/main/java/dev/manolovn/mergeksortedlists/MergeKSortedLists.java
@@ -1,7 +1,14 @@
 package dev.manolovn.mergeksortedlists;
 
+import dev.manolovn.common.ListNode;
+
 import java.util.PriorityQueue;
 
+/**
+ * source: https://leetcode.com/problems/merge-k-sorted-lists/
+ * difficulty: HARD
+ * topic: HEAP + LINKED LISTS
+ */
 public class MergeKSortedLists {
 
     /**
@@ -9,7 +16,7 @@ public class MergeKSortedLists {
      * Space: O(k)
      */
     public static ListNode merge(ListNode[] lists) {
-        PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.value - b.value);
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
         for (ListNode root : lists) {
             heap.offer(root);
         }
@@ -30,12 +37,34 @@ public class MergeKSortedLists {
         return head;
     }
 
-    static class ListNode {
-        int value;
-        ListNode next;
+    /*
+    The approach is to use a heap to track the minimum value of every list,
+    so we create a heap and store the k head ListNodes.
+    After that, we only need to poll to the heap and move the next pointers
+    to the next node when exists
+    Time:  O(nlogk)
+    Space: O(k)
+    ---
+    Another easy and optimal approach could be merge the lists 2 by 2 like
+    https://leetcode.com/problems/merge-two-sorted-lists/
+    */
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode head = new ListNode(0);
+        ListNode pointer = head;
 
-        ListNode(int value) {
-            this.value = value;
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((a,b) -> a.val - b.val);
+        for (ListNode ln : lists)
+            if (ln != null) heap.offer(ln);
+
+        while (!heap.isEmpty()) {
+            ListNode node = heap.poll();
+            pointer.next = node;
+            pointer = pointer.next;
+            node = node.next;
+            if (node != null)
+                heap.offer(node);
         }
+
+        return head.next;
     }
 }
