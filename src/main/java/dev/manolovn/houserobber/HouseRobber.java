92f6e3b9a8743d91282f922eef346c3184e7dd06
manolovn <manolo.vn@gmail.com>
2021-04-05 16:57:11 +0200

    solution added: house robber

--- src/main/java/dev/manolovn/houserobber/HouseRobber.java
+++ src/main/java/dev/manolovn/houserobber/HouseRobber.java
@@ -21,4 +21,25 @@ public class HouseRobber {
         }
         return curr;
     }
+
+    /**
+     * Time:  O(n)
+     * Space: O(1)
+     */
+    public int rob_alternative(int[] nums) {
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                even += nums[i];
+                even = Math.max(even, odd);
+            } else {
+                odd += nums[i];
+                odd = Math.max(even, odd);
+            }
+        }
+
+        return Math.max(even, odd);
+    }
 }
